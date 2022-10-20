282e44abba0fb50ba12508a1a04bee5dc697a139
Zamaal Azad <110616866+zamaalazad@users.noreply.github.com>
2022-10-20 13:45:30 +0300

    Responsive Menu and Site

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ const menu = document.querySelector('.main-menu');
 menuTarget.addEventListener('click', ()=>{
     menu.classList.toggle('isActive');
 });
+
+const closeTarget = document.getElementById('close');
+const menuMobile = document.getElementById('targetMobile');
+const mobileMenu = document.querySelector('.main-menu__mobile');
+
+menuMobile.addEventListener('click', ()=>{
+    mobileMenu.classList.add('isActiveMobile');
+});
+
+closeTarget.addEventListener('click', ()=>{
+    mobileMenu.classList.remove('isActiveMobile');
+})
